f94fcbc5eed14f906b2c19e860c00e411a406e22
Hunter2807 <46440327+Hunter2807@users.noreply.github.com>
2021-05-22 09:30:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from rsap.__init__ import __version__
 
-with open("README.md", "r") as file:
+with open("README.md", "r", encoding="utf-8") as file:
     long_description = file.read()
 
 
